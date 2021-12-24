10dc68514f0e0bfe5969fcee01a785f1aa55109b
Euler-JS <joaoalbertojose96@gmail.com>
2021-12-24 22:56:16 +0200

    listando doacoes, e nome do pacote do projecto alterado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Platform } from '@ionic/angular';
+import { AddDataService } from './services/add-data.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,50 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor(
+    dataService: AddDataService,
+    platform: Platform) {
+      try {
+      dataService.getDataFromStorage("ofertas")
+      } catch (error) {
+        console.log("Ocorreu um erro ao pegar lista das ofertas");
+        
+      }
+
+      try {
+      dataService.getDataFromStorage("dizimos") 
+      } catch (error) {
+        console.log("Ocorreu um erro ao pegar lista das dizimos");
+      }
+
+      try {
+        dataService.getDataFromStorage("doacoes")
+      } catch (error) {
+        console.log("Ocorreu um erro ao pegar lista das doacoes");
+      }
+
+      try {
+        dataService.getDataFromStorage("dispesasFixas")
+      } catch (error) {
+        console.log("Ocorreu um erro ao pegar lista das dispesas Fixas");
+      }
+
+      try {
+        dataService.getDataFromStorage("dispesasNaoFixas")
+      } catch (error) {
+        console.log("Ocorreu um erro ao pegar lista das dispesas nao fixas");
+      }
+      
+      
+      
+    platform.ready().then(preparado=>{
+      this.iniciar()
+    })
+  }
+
+  async iniciar()
+  {
+    console.log("Iniciando Yosef");
+    
+  }
 }
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { AddDataService } from '../services/add-data.service';
 
 @Component({
   selector: 'app-tab3',
@@ -7,6 +9,88 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  constructor() {}
+  doacaoObject=
+    {
+      valor:'',
+      nome:'',
+      data:'',
+      // eslint-disable-next-line @typescript-eslint/naming-convention
+      id_user: null
+    };
+
+    allDoacoes: any;
+  constructor(
+    public alertController: AlertController,
+    private addDataService: AddDataService
+  ) {
+    try {
+      addDataService.getDataFromStorage('doacoes');
+    } catch (error) {
+      console.log('Sem doacoes registadas');
+
+    }
+  }
+
+  ionViewDidEnter() {
+
+    try {
+      this.allDoacoes = this.addDataService.doacoesArray;
+    } catch (error) {
+      console.log('Array Vazio');
+
+    }
+
+  }
+
+  addDoacao()
+  {
+    this.doacaoAlert();
+  }
+
+  async doacaoAlert() {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Adiconar Oferta',
+      inputs: [
+        {
+          name: 'doacao',
+          type: 'number',
+          placeholder: 'Digite o valor doado.'
+        },
+        {
+          name: 'nome',
+          type: 'text',
+          placeholder: 'Digite o nome do doador'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+            console.log('Confirm Cancel');
+          }
+        }, {
+          text: 'Ok',
+          handler: (val) => {
+            const now = new Date();
+            console.log('Minha Data ',(new Date()).toISOString());
+            this.doacaoObject.valor = val.doacao;
+            this.doacaoObject.nome = val.nome;
+            this.doacaoObject.data = (new Date()).toISOString();
+            this.doacaoObject.id_user = 11;
+            this.addDataService.doacoesArray.push(this.doacaoObject);
+
+            this.addDataService.addDataOnStorage('doacoes',JSON.stringify(this.addDataService.doacoesArray));
+            console.log('Confirm Ok', val);
+            this.allDoacoes = this.addDataService.doacoesArray;
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
 
 }
