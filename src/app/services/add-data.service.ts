04bbbede9d81d80a3a15b236ecbe95ffb049f28c
Euler-JS <joaoalbertojose96@gmail.com>
2021-12-24 12:32:56 +0200

    adicionando dizimo

--- src/app/services/add-data.service.ts
+++ src/app/services/add-data.service.ts
@@ -5,44 +5,117 @@ import { Storage } from '@capacitor/storage';
   providedIn: 'root'
 })
 export class AddDataService {
-  ofertasArray:any = []
+  ofertasArray: any = []
+  dizimosArray: any = []
+  doacoesArray: any = []
+  dispesasFixasArray: any = []
+  dispesasNaoFixasArray: any = []
+  lastKey: any
   constructor() { }
 
-  async addDataOnStorage(name, object)
-  {
-    // await Storage.get({ key: 'YOSEF_S_'+name }).then(res=>
-    //   {
-    //     console.log("Valor ",JSON.parse(res.value));
-        
-    //   });
+  async addDataOnStorage(name, object) {
+    await Storage.set({
+      key: 'YOSEF_S_' + name,
+      value: object,
+    });
 
-    // alert(`Hello ${value}!`);
-   
-      await Storage.set({
-        key: 'YOSEF_S_'+name,
-        value: object,
-      });
+    switch (name) {
+      case "ofertas":
+        this.ofertasArray = JSON.parse(object);
+        break;
+      case "dizimos":
+        this.dizimosArray = JSON.parse(object);
+        break;
 
-      this.ofertasArray = JSON.parse(object);
-    
+      case "doacoes":
+        this.doacoesArray = JSON.parse(object);
+        break;
+
+      case "dispesasFixas":
+        this.dispesasFixasArray = JSON.parse(object);
+        break;
+
+      case "dispesasNaoFixas":
+        this.dispesasNaoFixasArray = JSON.parse(object);
+        break;
+
+      default:
+        break;
+    }
   }
 
-  async getDataFromStorage(name)
-  {
-    await Storage.get({ key: 'YOSEF_S_'+name }).then(res=>
-      {
-        console.log(JSON.parse(res.value));
-        if(res.value == null)
-        {
-          this.ofertasArray = []
-        }
-        else
-        {
-          this.ofertasArray = JSON.parse(res.value);
-        }
-        
-        
-      });
+  async getDataFromStorage(name) {
+
+
+    await Storage.get({ key: 'YOSEF_S_' + name }).then(res => {
+      console.log(JSON.parse(res.value));
+      switch (name) {
+        case "ofertas":
+          if (res.value == null) {
+            this.ofertasArray = []
+          }
+          else {
+            this.ofertasArray = JSON.parse(res.value);
+          }
+
+          break;
+        case "dizimos":
+          if (res.value == null) {
+            this.dizimosArray = []
+          }
+          else {
+            this.dizimosArray = JSON.parse(res.value);
+          }
+          break;
+
+        case "doacoes":
+          if (res.value == null) {
+            this.doacoesArray = []
+          }
+          else {
+            this.doacoesArray = JSON.parse(res.value);
+          }
+          break;
+
+        case "dispesasFixas":
+          if (res.value == null) {
+            this.dispesasFixasArray = []
+          }
+          else {
+            this.dispesasFixasArray = JSON.parse(res.value);
+          }
+          break;
+
+        case "dispesasNaoFixas":
+          if (res.value == null) {
+            this.dispesasNaoFixasArray = []
+          }
+          else {
+            this.dispesasNaoFixasArray = JSON.parse(res.value);
+          }
+          break;
+
+        default:
+          break;
+      }
+
+
+
+    });
+  }
+
+  async keyNumber() {
+    await Storage.get({ key: 'YOSEF_S_KEY' }).then(res => {
+      console.log(JSON.parse(res.value));
+      if (res.value == null) {
+        this.lastKey = 0;
+      }
+      else {
+        this.lastKey = parseInt(JSON.parse(res.value));
+      }
+
+
+    });
   }
 }
 
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { AddDataService } from '../services/add-data.service';
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +9,89 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  dizimoObject=
+    {
+      valor:'',
+      nome:'',
+      data:'',
+      // eslint-disable-next-line @typescript-eslint/naming-convention
+      id_user: null
+    };
+
+  allDizimos: any;
+  constructor(
+    public alertController: AlertController,
+    private addDataService: AddDataService
+  ) {
+    try {
+      addDataService.getDataFromStorage('dizimos');
+    } catch (error) {
+      console.log('Sem dizimos registadas');
+
+    }
+
+  }
+
+  ionViewWillEnter() {
+
+    try {
+      this.allDizimos = this.addDataService.dizimosArray;
+    } catch (error) {
+      console.log('Array Vazio');
+
+    }
+
+  }
+
+  addDizimo()
+  {
+    this.dizimoAlert();
+  }
+
+  async dizimoAlert() {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Adiconar Oferta',
+      inputs: [
+        {
+          name: 'dizimo',
+          type: 'number',
+          placeholder: 'Digite o valor do Dízimo'
+        },
+        {
+          name: 'nome',
+          type: 'text',
+          placeholder: 'Digite o nome do Dizimista'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+            console.log('Confirm Cancel');
+          }
+        }, {
+          text: 'Ok',
+          handler: (val) => {
+            const now = new Date();
+            console.log('Minha Data ',(new Date()).toISOString());
+            this.dizimoObject.valor = val.dizimo;
+            this.dizimoObject.nome = val.nome;
+            this.dizimoObject.data = (new Date()).toISOString();
+            this.dizimoObject.id_user = 11;
+            this.addDataService.dizimosArray.push(this.dizimoObject);
+
+            this.addDataService.addDataOnStorage('dizimos',JSON.stringify(this.addDataService.dizimosArray));
+            console.log('Confirm Ok', val);
+            this.allDizimos = this.addDataService.dizimosArray;
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
 
 }
